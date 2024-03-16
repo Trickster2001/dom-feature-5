77b3d522f9ebfdb8051ee835059fd4cee17b29e0
Trickster2001 <keyurmurkar@gmail.com>
2024-03-16 12:38:34 +0530

    completed 16/03/24

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("Hello World!");
+
+var arr = [
+  {dp:"/1.jpeg", story:"https://images.unsplash.com/photo-1626625158499-c6fbf99bd9ee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D"},
+  {dp:"https://plus.unsplash.com/premium_photo-1664464229692-44046bfd6b7d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZWx8ZW58MHx8MHx8fDA%3D", story:"https://images.unsplash.com/photo-1621235151131-a7efd0c0c501?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D"},
+  {dp:"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9kZWx8ZW58MHx8MHx8fDA%3D", story:"https://images.unsplash.com/photo-1709765737105-ef353471e267?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D"},
+  {dp:"https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bW9kZWx8ZW58MHx8MHx8fDA%3D", story:"https://plus.unsplash.com/premium_photo-1673356301861-d555ce5972ef?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bW9kZWx8ZW58MHx8MHx8fDA%3D"},
+  {dp:"https://images.unsplash.com/photo-1562572159-4efc207f5aff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D", story:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsfGVufDB8fDB8fHww"}
+];
+
+let clutter="";
+
+arr.forEach((ele, idx)=>{
+  clutter+=`<div class="story">
+  <img id="${idx}" src="${ele.dp}" alt="">        
+</div>`
+})
+
+let stories = document.querySelector("#stories");
+let fullScreen = document.getElementById("fullScreen")
+
+stories.innerHTML=clutter;
+
+stories.addEventListener("click", (dets)=>{
+  var value = arr[dets.target.id].story;
+  fullScreen.style.display="block";
+  fullScreen.style.backgroundImage=`url(${value})`
+  setTimeout(()=>{
+    fullScreen.style.display="none";
+  },2000)
+})
+
+fullScreen.addEventListener("click", ()=>{
+  fullScreen.style.display="none";
+})
